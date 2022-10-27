3d57b4b846e31ba0c0d039d0202349632093a77f
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2022-10-28 00:20:15 +0800

    Sanity integration

---
+++ pages/career/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Career() {
+  return (
+    <div>Testing 123</div>
+  )
+}
+
---
+++ pages/posts/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Posts() {
+  return (
+    <div>Posts 123</div>
+  )
+}
+
