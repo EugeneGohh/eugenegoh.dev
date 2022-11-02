3f858cac5af53b14b6d732c78917d3be22e87dff
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2022-11-02 11:04:55 +0800

    update schema & env var

--- studio/schemas/company.js
+++ studio/schemas/company.js
@@ -4,17 +4,59 @@ export default {
   title: "Company",
   fields: [
     {
-      name: "name",
+      name: "companyName",
       type: "string",
       title: "Company Name",
     },
     {
-      name: "image",
+      name: "companyLogo",
       type: "image",
       title: "Company Logo",
       option: {
         hotspot: true,
       },
+    },
+    {
+      name: "companyDescription",
+      type: "text",
+      title: "Company Description",
+    },
+    {
+      name: "companyUrl",
+      type: "url",
+      title: "Company Site",
+    },
+    {
+      name: "positionName",
+      type: "string",
+      title: "Position Name",
+    },
+    {
+      name: "positionType",
+      type: "string",
+      title: "Position Type",
+    },
+    {
+      name: "startingDate",
+      type: "date",
+      title: "Starting date",
+      options: {
+        dateFormat: "YYYY-MM",
+      },
+    },
+    {
+      name: "endingDate",
+      type: "date",
+      title: "Ending date",
+      options: {
+        dateFormat: "YYYY-MM",
+      },
+    },
+    {
+      name: "workingExperience",
+      type: "array",
+      title: "Working experience",
+      of: [{ type: "block" }],
     },
   ],
 };
