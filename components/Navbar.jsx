46f7f3199af11aa6637d064aed5e33f215b1fa1f
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2022-12-26 23:16:39 +0800

    make navbar sticky

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+    <div className="sticky top-0 z-30 overflow-hidden bg-white shadow sm:rounded-lg">
       <div className="px-4 py-7 sm:px-6">
         <h3 className="text-lg font-medium leading-6 text-gray-900">
           {`Eugene Goh`}
